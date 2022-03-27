73632a57fee442e4396a42659c33c5a6fcb50bbf
nikhill56 <61156011+nikhill56@users.noreply.github.com>
2022-03-27 16:57:28 +0530

    upto footer

--- Client/src/Components/Welcome.jsx
+++ Client/src/Components/Welcome.jsx
@@ -1,9 +1,10 @@
+import React, { useContext } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
-
+import { TransactionContext } from "../context/TransactionContext";
 import { Loader } from "./";
-
+import { shortenAddress } from "../utils/shortenAdddress";
 const commonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -19,13 +20,26 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-  const connectWallet = () => {};
+  const {
+    connectWallet,
+    currentAccount,
+    formData,
+    sendTransaction,
+    handleChange,
+  } = useContext(TransactionContext);
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    const { addressTo, amount, message } = formData;
+    e.preventDefault();
+
+    if (!addressTo || !amount || !message) return;
+
+    sendTransaction();
+  };
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col md:mr-10">
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto
             <br />
@@ -34,13 +48,17 @@ const Welcome = () => {
           <p className="text-left text-white mt-5 font-light md:w-9/12 w-11/12 text-base">
             Buy and Sell Crypto
           </p>
-          <button
-            type="button"
-            onClick={connectWallet}
-            className="flex flex-row justify-center items-center my-5 bg-[#53f0a7] p-3 rounded-full cursor-pointer hover:bg-gradient-to-r from-blue-500 to-blue-700"
-          >
-            <p className="text-white text-base font-semibold">Connect Wallet</p>
-          </button>
+          {!currentAccount && (
+            <button
+              type="button"
+              onClick={connectWallet}
+              className="flex flex-row justify-center items-center my-5 bg-[#53f0a7] p-3 rounded-full cursor-pointer hover:bg-gradient-to-r from-blue-500 to-blue-700"
+            >
+              <p className="text-white text-base font-semibold">
+                Connect Wallet
+              </p>
+            </button>
+          )}
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
             <div className={commonStyles}>Security</div>
@@ -51,7 +69,7 @@ const Welcome = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+      <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
         <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism ">
           <div className="flex justify-between flex-col w-full h-full ">
             <div className="flex justify-between items-start">
@@ -61,7 +79,9 @@ const Welcome = () => {
               <BsInfoCircle className="text-white text-1xl" />
             </div>
             <div>
-              <p className="text-white font-light text-sm">ADDRESS</p>
+              <p className="text-white font-light text-sm">
+                {shortenAddress(currentAccount)}
+              </p>
               <p className="text-white font-semibold text-lg mt-1">ETHEREUM</p>
             </div>
           </div>
@@ -71,19 +91,19 @@ const Welcome = () => {
             placeholder="Address To"
             name="addressTo"
             type="text"
-            handleChange={() => {}}
+            handleChange={handleChange}
           />
           <Input
             placeholder="Amount (ETH)"
             name="amount"
             type="number"
-            handleChange={() => {}}
+            handleChange={handleChange}
           />
           <Input
             placeholder="Enter Message"
             name="message"
             type="text"
-            handleChange={() => {}}
+            handleChange={handleChange}
           />
           <div className="h-[1px] w-full b-gray my-2" />
           {false ? (
---
+++ Client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+
+export const contractAddress = "0xBB38c1568CC2A7141c187A1886EBB1bf2515202d";
